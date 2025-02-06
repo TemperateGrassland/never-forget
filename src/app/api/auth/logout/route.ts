413ba0e46fb20bcab2ce46875d6af5647bd7477a
Charlie Allen <charlie06allen@gmail.com>
2025-02-06 09:03:53 +0000

    delete logout route

--- src/app/api/auth/logout/route.ts
+++
@@ -1,8 +0,0 @@
-import { NextResponse } from "next/server";
-
-export async function GET() {
-  console.log("🔹 logout callback triggered");
-  const response = NextResponse.redirect("https://localhost:3000/");
-  response.cookies.set("next-auth.session-token", "", { expires: new Date(0) });
-  return response;
-}
