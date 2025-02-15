7e458b853d97a8b1ef466a18f8dcdf735930a3ea
Charlie Allen <charlie06allen@gmail.com>
2025-02-15 17:32:35 +0000

    remove session provider file - can just import from next auth instead

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,9 +1,6 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-// import { getServerSession } from "next-auth";
-// import AuthProvider from "./components/ui/AuthProvider";
-// import NavMenu from "./components/ui/NavMenu";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
