70be3002a6fbfe5320f8fe27bf29bbe50d7be892
Charlie Allen <charlie06allen@gmail.com>
2025-02-15 11:18:26 +0000

    tidy up the auth file

--- src/auth.ts
+++ src/auth.ts
@@ -1,11 +1,7 @@
-// "use server"
-
 import NextAuth from 'next-auth';
 import Nodemailer from "next-auth/providers/nodemailer"
 import { PrismaAdapter } from "@next-auth/prisma-adapter";
 import { PrismaClient } from "@prisma/client";
-// import { Session } from "next-auth";
-// import type { DefaultJWT } from 'next-auth/core/types';
 
 const prisma = new PrismaClient();
 
@@ -14,13 +10,13 @@ export const { auth, handlers, signIn, signOut } = NextAuth({
     adapter: PrismaAdapter(prisma),
     providers: [
       Nodemailer({
-          server: process.env.EMAIL_SERVER, // SMTP server configuration
-          from: process.env.EMAIL_FROM,    // Email address used to send magic links
+          server: process.env.EMAIL_SERVER, 
+          from: process.env.EMAIL_FROM,   
         }),
       ],
       secret: process.env.NEXTAUTH_SECRET, // Used for signing/encrypting JWTs and cookies
       session: {
-        strategy: "database", // ✅ Ensure it is "database" (not "jwt")
+        strategy: "database", 
     },
     debug: true,
     // callbacks: { // ✅ Move jwt and session under callbacks
