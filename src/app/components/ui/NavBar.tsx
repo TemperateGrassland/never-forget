b59f2c02aa4384a70a95114b0f2e98a956130a30
Charlie Allen <charlie06allen@gmail.com>
2025-02-20 08:24:57 +0000

    re-adjust nav bar centering

--- src/app/components/ui/NavBar.tsx
+++ src/app/components/ui/NavBar.tsx
@@ -8,18 +8,17 @@ const Navbar: React.FC = () => {
 
   return (
     <nav className="bg-blue-500 p-4 shadow-md">
-      <div className="container mx-auto flex justify-between items-center">
-        <div className="text-white text-xl font-bold">
+      <div className="container mx-auto flex items-center justify-between relative">
+        {/* Left Spacer to balance the center */}
+        <div className="w-1/3"></div>
+
+        {/* Centered Title */}
+        <div className="text-white text-xl font-bold absolute left-1/2 transform -translate-x-1/2">
           <Link href="/">Never Forget</Link>
         </div>
 
-        {/* Hamburger Button (Mobile) */}
-        <button className="text-white md:hidden" onClick={() => setIsOpen(!isOpen)}>
-          ☰
-        </button>
-
-        {/* Navigation Links */}
-        <ul className={`md:flex space-x-6 ${isOpen ? "block" : "hidden"} md:block`}>
+        {/* Navigation Links (Right Aligned) */}
+        <ul className="flex space-x-6 w-1/3 justify-end">
           <li>
             <Link href="/" className="text-white hover:underline">
               Home
