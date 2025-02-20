1b284406f3703115870768ce7e3891397c7d8c44
Charlie Allen <charlie06allen@gmail.com>
2025-02-20 08:35:52 +0000

    update text in login button component

--- src/app/components/ui/SignIn.tsx
+++ src/app/components/ui/SignIn.tsx
@@ -4,6 +4,6 @@ import { signIn } from "next-auth/react";
 
 export function LoginButton() {
   return (
-  <button onClick={() => signIn("/dashboard")}>Sign in with Email</button>
+  <button onClick={() => signIn("/dashboard")}>Sign in</button>
   )
 }
