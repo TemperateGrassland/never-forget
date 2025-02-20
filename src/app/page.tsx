f4e3a61ff56d6303dfb05cd52c93884db8373476
Charlie Allen <charlie06allen@gmail.com>
2025-02-20 08:48:19 +0000

    update tag line on home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -24,7 +24,7 @@ export default async function Page() {
   }
   return (
     <>
-    <h1 className="text-3xl font-bold text-center mt-4">Hello!</h1>
+    <h1 className="text-3xl font-bold text-center mt-4">Never forget an important date again!</h1>
     <div className="flex flex-col items-center mt-4">
       <p className="text-lg text-red-600 font-semibold bg-red-100 px-4 py-2 rounded-md shadow-md">
         ❌ You are not signed in. Please log in below.
