2d75b5430e8e2da17215e86e1585e30b4f6f63c3
Charlie Allen <charlie06allen@gmail.com>
2025-02-22 12:18:09 +0000

    set callback url for auth redirection to /dashboard

--- src/app/components/ui/SignIn.tsx
+++ src/app/components/ui/SignIn.tsx
@@ -4,7 +4,7 @@ import { signIn } from "next-auth/react";
 
 export function LoginButton() {
   return (
-  <button onClick={() => signIn("/dashboard")}
+  <button onClick={() => signIn("email", { callbackUrl: "/dashboard" })}
   className="bg-blue-500 hover:bg-blue-600 text-white font-semibold px-4 py-2 rounded-md shadow-md transition duration-300"
   >
     Sign in</button>
