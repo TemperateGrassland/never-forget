ded400b83b8985ae4ddb32b00db5840870d07e65
Charlie Allen <charlie06allen@gmail.com>
2025-03-01 09:24:35 +0000

    update env var refs

--- src/app/components/ui/CheckoutButton.tsx
+++ src/app/components/ui/CheckoutButton.tsx
@@ -4,7 +4,7 @@ import React from "react";
 import { useStripe, useElements, Elements } from "@stripe/react-stripe-js";
 import { loadStripe } from "@stripe/stripe-js";
 
-const stripePromise = loadStripe(process.env.NEXT_PUBLIC_STRIPE_PUBLISHABLE_KEY!);
+const stripePromise = loadStripe(process.env.STRIPE_PUBLISHABLE_KEY!);
 
 export default function CheckoutButton() {
   const stripe = useStripe();
--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -9,7 +9,7 @@ import CheckoutButton from "../components/ui/CheckoutButton";
 import { Elements } from "@stripe/react-stripe-js";
 import { loadStripe } from "@stripe/stripe-js";
 
-const stripePromise = loadStripe(process.env.NEXT_PUBLIC_STRIPE_PUBLISHABLE_KEY!);
+const stripePromise = loadStripe(process.env.STRIPE_PUBLISHABLE_KEY!);
 
 
 export default async function DashboardPage() {
