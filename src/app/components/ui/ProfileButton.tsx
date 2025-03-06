4775f9405bcfe10b238bbe8af1a841b53212ee95
Charlie Allen <charlie06allen@gmail.com>
2025-03-06 08:51:13 +0000

    add missing return statement in ViewProfile()

--- src/app/components/ui/ProfileButton.tsx
+++ src/app/components/ui/ProfileButton.tsx
@@ -3,7 +3,9 @@
 import Link from "next/link";
 
 export default function ViewProfile() {
-    <button className="bg-blue-500 text-white px-4 py-2 rounded hover:bg-blue-700">
-      <Link href="/profile">View Profile</Link>
-    </button>
+    return(
+        <button className="bg-blue-500 text-white px-4 py-2 rounded hover:bg-blue-700">
+        <Link href="/profile">View Profile</Link>
+        </button>
+    )
 }
