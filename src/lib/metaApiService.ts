03d8c39dd8110566ddeac377411eba0eae4d29d9
Charlie Allen <charlie06allen@gmail.com>
2025-03-16 14:42:45 +0000

    dynamically create reminders

--- src/lib/metaApiService.ts
+++ src/lib/metaApiService.ts
@@ -14,17 +14,75 @@ export async function buildMetaApiRequests() {
   console.log("usersWithReminders:", usersWithReminders)
 
   // Format the data into JSON payloads for Meta API
-  return usersWithReminders.map((user) => ({
+  return usersWithReminders.map((user) => (
+    {
+        messaging_product: 'whatsapp',
+        to: user.phoneNumber,
+        type: 'template',
+        template: {
+            name: 'reminder',
+            language: {
+                code: 'en_GB',
+                policy: 'deterministic'
+            },
+            components: [
+                {
+                    type: 'body',
+                    parameters: [
+                        {
+                            "type": "text",
+                            "parameter_name": "text",
+                            "text": user.firstName
+                        },
+                        {
+                            "type": "text",
+                            "parameter_name": "reminder_1",
+                            "text": "Drive to Winslow"
+                        },
+                        {
+                            "type": "text",
+                            "parameter_name": "when_1",
+                            "text": "Today"
+                        },
+                        {
+                            "type": "text",
+                            "parameter_name": "reminder_2",
+                            "text": "Take care of Simbi and Chai"
+                        },
+                        {
+                            "type": "text",
+                            "parameter_name": "when_2",
+                            "text": "Always"
+                        },
+                        {
+                            "type": "text",
+                            "parameter_name": "reminder_3",
+                            "text": "Get shit together"
+                        },
+                        {
+                            "type": "text",
+                            "parameter_name": "when_3",
+                            "text": "Maybe tomorrow?"
+                        }
+                    ]
+                }
+            ]
+        }
+    }
+    
+    
+    
+    
+    {
     messaging_product: 'whatsapp',
-    recipient_type: 'individual',
-    to: user.phoneNumber, // Send to user's phone number
+    to: user.phoneNumber, 
     type: 'template',
     template: {
-      name: 'reminder_notification',
-      language: { code: 'en_US' },
+      name: 'reminder',
+      language: { code: 'en_GB' },
       components: [
         {
-          type: 'body',
+          type: 'text',
           parameters: user.reminders.map((reminder) => ({
             type: 'text',
             text: `${reminder.title}: ${reminder.description || 'No description'}`,
