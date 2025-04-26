2b9dca9a485b364c0e45f39a1d7fa36eb9db73d6
Charlie Allen <charlie06allen@gmail.com>
2025-04-26 16:47:44 +0100

    add apikey env var to the nextauth mailgun provider

--- src/auth.ts
+++ src/auth.ts
@@ -12,6 +12,7 @@ export const { auth, handlers, signIn, signOut } = NextAuth({
     adapter: PrismaAdapter(prisma),
     providers: [
       Mailgun({
+          apiKey: process.env.MAILGUN_API_KEY,
           server: process.env.EMAIL_SERVER, 
           from: process.env.EMAIL_FROM,   
         }),
