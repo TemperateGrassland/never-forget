7bac6197af0f3e05299607576d3a40e471906e48
Charlie Allen <charlie06allen@gmail.com>
2025-05-07 21:06:39 +0100

    set text-black in privacy and aboutus pages

--- src/app/aboutus/page.tsx
+++ src/app/aboutus/page.tsx
@@ -1,7 +1,7 @@
 export default function AboutUsPage() {
     return (
         <>
-        <main className="prose prose-black max-w-3xl mx-auto p-8">
+        <main className="text-black max-w-3xl mx-auto p-8">
         <h1>Privacy Policy</h1>
         <p><em>Last updated: [Date]</em></p>
         <hr />
--- src/app/privacy/page.tsx
+++ src/app/privacy/page.tsx
@@ -1,7 +1,7 @@
 export default function PrivacyPage() {
   return (
       <>
-      <main>
+        <main className="text-black max-w-3xl mx-auto p-8">
         <h1>Privacy Policy</h1>
         <p><em>Last updated: [Date]</em></p>
         <hr />
