16e3c1cd5df0d822050fa3ccf5d20605b4f4616a
Charlie Allen <charlie06allen@gmail.com>
2025-05-09 08:54:54 +0100

    update privacy page

--- src/app/privacy/page.tsx
+++ src/app/privacy/page.tsx
@@ -2,8 +2,52 @@ export default function PrivacyPage() {
   return (
       <>
         <main className="text-black max-w-3xl mx-auto p-8">
-        <h1>Privacy Policy placeholder</h1>
-        </main>
-      </>
-  );
-}
+        <h1>Privacy Policy for never forget</h1>
+
+  Effective Date: September 09, 2025
+
+  never forget (https://datafa.st) values your privacy. This policy explains how we collect, use, and protect your information.
+
+  1. Information We Collect
+
+    •	Personal Data: We collect email addresses when you sign up for a free trial or subscription. This data is used to manage your account, communicate with you, and provide access to our services.
+
+  2. Use of Information
+
+    •	Personal Data: Your email address is used for account management, service-related communications, and providing access to our platform.
+
+  3. Sharing of Data
+
+  We do not sell, trade, or share your email address or other personal data with third parties, except as necessary to provide services (e.g., payment processors) or comply with legal obligations.
+
+  4. Cookies
+
+  We use cookies to track website usage and improve your experience. These cookies collect non-personal data, such as visitor activity, for analytics. You are responsible for obtaining consent from your website visitors before collecting cookies, as required by GDPR. You can disable cookies in your browser settings, but doing so may limit your access to certain features.
+
+  5. Security
+
+  We take reasonable steps to protect your personal data, including email addresses, and non-personal data from unauthorized access, disclosure, or misuse. However, no method of transmission over the internet is 100% secure.
+
+  6. User Rights
+
+  You may request to access, modify, or delete your email address or other personal data at any time by contacting us at charlie@neverforget.one.
+
+  7. Data Retention
+
+  If you do not subscribe after the free trial, all user analytics and collected non-personal data will be deleted a few days after the trial period ends. Your email address may be retained for future communication unless you request its deletion.
+
+  8. Updates to Privacy Policy
+
+  We may update this policy from time to time. You will be notified of any changes via email.
+
+  9. Governing Law
+
+  This Privacy Policy is governed by the laws of England and Wales.
+
+  10. Contact Information
+
+  For questions about this policy, please contact us at charlie@neverforget.one.
+          </main>
+        </>
+    );
+  }
