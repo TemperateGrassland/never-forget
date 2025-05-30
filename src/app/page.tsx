6ba6e80f01ee265c02ee7ff58f7c79d0127dab90
Charlie Allen <charlie06allen@gmail.com>
2025-05-30 07:41:02 +0100

    update the NF headline

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -9,7 +9,7 @@ export default async function Page() {
   return (
     <>
     <div className="flex flex-col items-center text-black px-4 sm:px-8 lg:px-16 max-w-screen-2xl mx-auto pb-16">
-      <h1 className="mb-6 text-lg font-agrandir text-center">Daily actionable reminders to your Whatsapp — join the waitlist now.</h1>
+      <h1 className="mb-6 text-lg font-agrandir text-center">Create a list of daily actionable reminders and have them sent to your Whatsapp every day! Join the waitlist now.</h1>
       <div className="w-full flex justify-center mb-4 sm:mb-6">
       <WaitlistButton />
       </div>
