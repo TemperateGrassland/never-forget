29208f42dca98487f331955e24d7a7fd9737ccdb
Charlie Allen <charlie06allen@gmail.com>
2025-06-12 10:09:02 +0900

    adjust the text on the landing page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -16,15 +16,18 @@ export default async function Page() {
     <>
     <div className="w-full min-h-screen flex justify-center">
       <div className="flex flex-col items-center text-black px-4 sm:px-8 lg:px-16 max-w-screen-2xl w-full mx-auto pb-16 text-center">
-        <h1 className="w-full max-w-3xl mb-6 text-xl sm:text-2xl lg:text-3xl font-agrandir text-center">
+        <h1 className="w-full max-w-3xl mb-6 text-4xl sm:text-2xl lg:text-3xl font-agrandir text-center">
           Create a list of daily actionable reminders and have them sent to your Whatsapp! 
-          <br />
-          Join the waitlist now and we will send you a notification when the service is ready.
         </h1>
         <div className="w-full flex justify-center mb-4 sm:mb-6">
           <div className="w-full max-w-md">
             <WaitlistButton />
           </div>
+        </div>
+        <div className="w-full max-w-3xl mb-6 text-xl sm:text-2xl lg:text-3xl font-agrandir text-center">
+          <h1>
+            Join the waitlist now and we will send you a notification when the service is ready.
+          </h1>
         </div>
         <div className="flex flex-col lg:flex-row items-center justify-center gap-4 w-full mt-8">
           <div className="w-full lg:w-2/3">
