88e74737d64e16c7e6622406a7b705797d7521a6
Charlie Allen <charlie06allen@gmail.com>
2025-10-22 09:14:28 +0100

    fix failed migration

--- prisma/migrations/20251022080907_remove_template_name_col/migration.sql
+++ prisma/migrations/20251022080907_remove_template_name_col/migration.sql
@@ -4,8 +4,8 @@
   - You are about to drop the column `templateName` on the `FlowResponse` table. All the data in the column will be lost.
 
 */
--- DropIndex
-DROP INDEX "FlowResponse_templateName_createdAt_idx";
+-- DropIndex (only if it exists)
+DROP INDEX IF EXISTS "FlowResponse_templateName_createdAt_idx";
 
--- AlterTable
-ALTER TABLE "FlowResponse" DROP COLUMN "templateName";
+-- AlterTable (only if column exists)
+ALTER TABLE "FlowResponse" DROP COLUMN IF EXISTS "templateName";
